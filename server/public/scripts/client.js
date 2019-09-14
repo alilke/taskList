fec9d060cd34ce24226e882b046c9405c538dc26
alilke <angelalilke@gmail.com>
2019-09-14 14:19:57 -0500

    working post and get

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,101 @@
 console.log( 'js' );
+$(document).ready(handleReady);
+
+function handleReady(){
+    setDate();
+    $('#submit').on('click', addTask);
+    getTasks();
+}
+
+function setDate(){
+    var now = new Date();
+    var month = (now.getMonth() + 1);               
+    var day = now.getDate();
+    if (month < 10) 
+        month = "0" + month;
+    if (day < 10) 
+        day = "0" + day;
+    var today = now.getFullYear() + '-' + month + '-' + day;
+    $('#dueDate').val(today);
+};
+
+function addTask(){
+    //GRABS TASK FROM INPUT FIELDS
+    const task =$('#task').val();
+    const dueDate =$('#dueDate').val();
+    const category =$('#category').val();
+    const notes =$('#notes').val();
+    const status =$('#status').val();
+    //TURNS  TASK INTO AN OBJECT
+    const objectToSend ={
+        task: task,
+        dueDate: dueDate,
+        category: category,
+        notes: notes,
+        status: status
+    }
+    //POST TASK TO THE SERVER
+    $.ajax({
+        method: 'POST',
+        url:'/tasks',
+        data: objectToSend
+    }).then(function(response){
+    //BRINGS BACK INFO FROM SERVER &
+    //DISPLAYS NEW TASK ON DOM
+        getTasks();
+    }).catch(function(err){
+        console.log(err);
+        alert('something wrong in the POST');       
+    })
+}
+
+function getTasks(){
+    //GET ANY TASKS ON THE SERVER 
+    $.ajax({
+        method: 'GET',
+        url:'/tasks'
+    }).then(function(response){
+        console.log('in getTasks response', response);
+        // APPEND TASKS TP THE DOM
+        appendTasks(response);
+    }).catch(function(err){
+        console.log('in getTasks - error', err);
+        alert('something went wrong in GET')     
+    })
+}
+
+function appendTasks(tasks){
+    //EMPTY OLD DATA
+    $('#taskDisplay').empty();
+    for(let task of  tasks){
+        console.log(task);
+        let tableRow = $(`
+        <tr>
+            <td>${task.task}</td>
+            <td>${task.dueDate}</td>
+            <td>${task.category}</td>
+            <td>${task.notes}</td>
+            <td>${task.status}</td>
+            <td><button data-id="${task.id}" class="deleteThis">Delete</button></td> 
+        </tr>`)
+        //ATTACH DATA TO ROW, NEED FOR  DELETE
+        tableRow.data('id',task.id);
+        //APPEND ROW TO TABLE
+        $('#taskDisplay').append(tableRow);        
+    }
+    $(`.deleteThis`).on('click', deleteBtn);
+}
+
+function deleteBtn(){
+    let el  =$(this).data('id');
+    console.log('delete', el);
+    $.ajax({
+        method: 'DELETE',
+        url: `/tasks/${el}`
+    }).then(function(response){
+        console.log(response);
+        getTasks();
+    }).catch(function(err){
+        alert('error on delete', err);
+    })   
+}
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,11 +2,46 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 // DB CONNECTION
-
-//GET
 //POST
-//PUT
+router.post('/',function(req, res){
+    const  taskToAdd  =  req.body; // this is data we are sending
+    console.log('in post route - task', taskToAdd);
+    const queryText  = 'INSERT INTO "toDo"("task", "dueDate", "category", "notes", "completed") VALUES ($1, $2, $3,  $4, $5);';
+    pool.query(queryText, [taskToAdd.task,  taskToAdd.dueDat, taskToAdd.category, taskToAdd.notes, taskToAdd.status]).then(()=>{
+        res.sendStatus(201);
+    }).catch((err)=>{
+        console.log('error in POST', err);       
+    });
+});  // END POST ROUTE
+//GET
+router.get('/', function (req, res) {
+console.log('In GET route');
+const queryText = 'SELECT * FROM "toDo";';
+pool.query(queryText).then((results) => {
+    console.log(results); // This is an object
+    res.send(results.rows); // result.rows is an Array of tasks
+}).catch((err) => {
+    console.log('Error making GET', err);
+    res.sendStatus(500);
+});
+}); // END GET ROUTE
+
 //DELETE
+router.delete('/:id', (req, res) => {
+    console.log('params',req.params.id);
+    let queryText =`DELETE FROM "toDo" WHERE "id" = '${req.params.id}';`;
+    pool.query(queryText)
+    .then((result)=>{
+    res.send(result.rows)        
+    })
+    .catch((err) =>{
+        console.log('error making queury', err);
+        res.sendStatus(500);
+    })   
+})
+
+//PUT
+
 
 
 
